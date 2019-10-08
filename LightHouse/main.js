37e5dbcd9880f2082a5172c298f585a7c0504986
Jung <iveinvalue@gmail.com>
2019-10-08 16:48:19 +0900

    reflect

--- LightHouse/main.js
+++ LightHouse/main.js
@@ -31,79 +31,100 @@ function object() {
     drawFAN(0, vertices.length)
     //sky end
 
-    //moon start
-    renderCircle(0.1, -0.46, 0.26, white, 1, 1)
-    //moon end
-
     //cloud start
     var color = vec4(0.23, 0.29, 0.41, 1);
     makeCloud(0.15, 0.74, color)
     makeCloud(-0.76, 0.5, color)
     //cloud end
 
-    //light start
     var tmp = false
-    if ((LSizeF && LightVec == 1) || (!LSizeF && LightVec == -1)) { 
-        tmp = true  
+    if ((LSizeF && LightVec == 1) || (!LSizeF && LightVec == -1)) {
+        tmp = true
         LightBack()
     }
-    moveLight()
-    if ((!LSizeF && LightVec == 1) || (!tmp && LightVec == -1)) { 
-        LightBack()
-    }
+    
     //light start
+    moveLight(false)
+    //light end
 
-    window.requestAnimationFrame(object); //setTimeout(object, 100)
+    if ((!LSizeF && LightVec == 1) || (!tmp && LightVec == -1)) {
+        LightBack()
+    }
+
+    window.requestAnimationFrame(object);
+    //setTimeout(object, 100)
 }
 
 //버튼 리스너
 function addListener() {
     document.getElementById("rotate").onclick = function (event) {
-        if(LightVec == 1){
+        if (LightVec == 1) {
             LightVec = -1
-        }else{
+        } else {
             LightVec = 1
         }
     };
 }
 
 function LightBack() {
-    //black start
-    var color = vec4(0, 0, 0, 1);
-    var vertices = [vec2(-1, -1), vec2(-1, -0.3), vec2(1, -0.3), vec2(1, -1),];
-    makeBuffer(vertices, makeColor(color, vertices.length))
-    drawFAN(0, vertices.length)
-    //black end
+    mainvec(false)
+    mainvec(true)
+}
+
+function mainvec(reverse) {
+
+    if (!reverse) {
+        //black start
+        var color = vec4(0.26, 0.38, 0.53, 1);
+        var vertices = [vec2(-1, -1), vec2(-1, -0.3), vec2(1, -0.3), vec2(1, -1),];
+        makeBuffer(vertices, makeColor(color, vertices.length))
+        drawFAN(0, vertices.length)
+        //black end
+    }
 
     //ground start
     var color = vec4(0.11, 0.18, 0.29, 1);
     var vertices = [vec2(-1, -0.15), vec2(-0.5, -0.1), vec2(1, -0.15), vec2(1, -0.3), vec2(-1, -0.3)];
-    makeBuffer(vertices, makeColor(color, vertices.length))
+    makeBuffer(vertices, makeColor(color, vertices.length), reverse)
     drawFAN(0, vertices.length)
     //ground end
 
     //lighthouse start
     var color = vec4(0.11, 0.18, 0.29, 1);
     var vertices = [vec2(0.3, -0.2), vec2(0.6, -0.2), vec2(0.55, 0.6), vec2(0.35, 0.6)];
-    makeBuffer(vertices, makeColor(color, vertices.length))
+    makeBuffer(vertices, makeColor(color, vertices.length), reverse)
     drawFAN(0, vertices.length)
     var vertices = [vec2(0.32, 0.6), vec2(0.58, 0.6), vec2(0.58, 0.66), vec2(0.32, 0.66)];
-    makeBuffer(vertices, makeColor(color, vertices.length))
+    makeBuffer(vertices, makeColor(color, vertices.length), reverse)
     drawFAN(0, vertices.length)
     var vertices = [vec2(0.41, 0.66), vec2(0.49, 0.66), vec2(0.49, 0.70), vec2(0.41, 0.70)];
-    makeBuffer(vertices, makeColor(color, vertices.length))
+    makeBuffer(vertices, makeColor(color, vertices.length), reverse)
     drawFAN(0, vertices.length)
     //lighthouse end
 
     //lighthouse light start
     var color = vec4(0.95, 0.96, 0.58, 1);
     var vertices = [vec2(0.38, 0.56), vec2(0.44, 0.56), vec2(0.44, 0.43), vec2(0.37, 0.43)];
-    makeBuffer(vertices, makeColor(color, vertices.length))
+    makeBuffer(vertices, makeColor(color, vertices.length), reverse)
     drawFAN(0, vertices.length)
     var vertices = [vec2(0.46, 0.43), vec2(0.53, 0.43), vec2(0.52, 0.56), vec2(0.46, 0.56)];
-    makeBuffer(vertices, makeColor(color, vertices.length))
+    makeBuffer(vertices, makeColor(color, vertices.length), reverse)
     drawFAN(0, vertices.length)
     //lighthouse light end
+
+    //moon start
+    renderCircle(0.1, -0.46, 0.26, white, 1, 1, reverse)
+    //moon end
+
+    if (reverse) {
+        //black start
+        var color = vec4(0, 0, 0, 0.7);
+        var vertices = [vec2(-1, -1), vec2(-1, -0.3), vec2(1, -0.3), vec2(1, -1),];
+        makeBuffer(vertices, makeColor(color, vertices.length))
+        drawFAN(0, vertices.length)
+        //black end
+    }
+
 }
 
 //구름
--- LightHouse/util.js
+++ LightHouse/util.js
@@ -15,13 +15,21 @@ function drawTRI(a, b) {
 }
 
 //버퍼 만들기
-function makeBuffer(vertices, colors) {
+function makeBuffer(vertices, colors, reverse) {
+    var tmpv = vertices
+
+    if (reverse) {
+        tmpv.forEach(function (element, index, array) {
+            tmpv[index][1] = element[1] * -1 - 0.6
+        });
+    }
+
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
     var vertexPositionBufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(tmpv), gl.STATIC_DRAW);
 
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
@@ -37,7 +45,7 @@ function makeBuffer(vertices, colors) {
 }
 
 //draw circle
-function renderCircle(r, x, y, color, subAngle, size) {
+function renderCircle(r, x, y, color, subAngle, size, reverse) {
     var noOfFans = 200;
     var centerOfCircle = vec2(x, y);
     var anglePerFna = (2 * Math.PI) / noOfFans;
@@ -52,17 +60,27 @@ function renderCircle(r, x, y, color, subAngle, size) {
             )
         );
     }
-    makeBuffer(mVirtices, makeColor(color, mVirtices.length))
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, mVirtices.length);
+
+    if (reverse) {
+        makeBuffer(mVirtices, makeColor(color, mVirtices.length), true)
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, mVirtices.length);
+    } else {
+        makeBuffer(mVirtices, makeColor(color, mVirtices.length))
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, mVirtices.length);
+    }
 }
 
 //빛 움직임
-function moveLight() {
-    var yelloT = vec4(1, 1, 0, 0.45);
-    renderCircle(3, 0.44, 0.48, yelloT, Math.PI * LAngle / 2 * LightVec, LSize)
+function moveLight(reverse) {
 
+    if (reverse) {
+        var yelloT = vec4(1, 1, 0, 0.1);
+        renderCircle(3, 0.44, 0.48, yelloT, Math.PI * LAngle / 2 * LightVec, LSize, true)
+    } else {
+        var yelloT = vec4(1, 1, 0, 0.45);
+        renderCircle(3, 0.44, 0.48, yelloT, Math.PI * LAngle / 2 * LightVec, LSize)
+    }
     //console.log(LSizeF);
-
     if (LSize == 10) {
         LSizeF = false
     }
@@ -89,6 +107,8 @@ function moveLight() {
         LAngleF = false
         LSize = 1
     }
+
+    
 }
 
 //마우스 리스너
@@ -106,5 +126,5 @@ function onMouseUp(event) {
     if (my < 250) { my = (250 - my) / 250 }
     if (my > 250) { my = (my - 250) / 250 * -1 }
     var tmp = "&nbspx: " + mx + " y: " + my
-    document.getElementById("location").innerHTML=tmp;
+    document.getElementById("location").innerHTML = tmp;
 }
