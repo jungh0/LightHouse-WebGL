43adae7a3306b521568ba69e19b4c095eb732dc8
Jung <iveinvalue@gmail.com>
2019-10-08 14:49:11 +0900

    light_done_file_name

--- LightHouse/main.js
+++ LightHouse/main.js
@@ -3,7 +3,7 @@ var points;
 
 var white = vec4(1, 1, 1, 1);
 
-var LightVec = 1 //등대 방향 바꾸기 1: 왼쪽 -1: 오른쪽
+var LightVec = -1 //등대 방향 바꾸기 1: 왼쪽 -1: 오른쪽
 var LAngle = 1
 var LAngleF = true
 var LSize = 2
@@ -18,11 +18,12 @@ window.onload = function init() {
     gl.enable(gl.BLEND);
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-    window.requestAnimationFrame(Ani);
+    window.requestAnimationFrame(object);
     canvas.addEventListener("mousemove", function () { onMouseUp(event) });
+    addListener()
 };
 
-function Ani() {
+function object() {
     //sky start
     var color = vec4(0.26, 0.38, 0.53, 1);
     var vertices = [vec2(-1, 1), vec2(-1, -0.3), vec2(1, -0.3), vec2(1, 1),];
@@ -41,13 +42,29 @@ function Ani() {
     //cloud end
 
     //light start
-    if ((LSizeF && LightVec == 1) || (!LSizeF && LightVec == -1)) { LightBack() }
+    var tmp = false
+    if ((LSizeF && LightVec == 1) || (!LSizeF && LightVec == -1)) { 
+        tmp = true  
+        LightBack()
+    }
     moveLight()
-    if ((!LSizeF && LightVec == 1) || (LSizeF && LightVec == -1)) { LightBack() }
+    if ((!LSizeF && LightVec == 1) || (!tmp && LightVec == -1)) { 
+        LightBack()
+    }
     //light start
 
-    window.requestAnimationFrame(Ani);
-    //setTimeout(Ani, 100)
+    window.requestAnimationFrame(object); //setTimeout(object, 100)
+}
+
+//버튼 리스너
+function addListener() {
+    document.getElementById("rotate").onclick = function (event) {
+        if(LightVec == 1){
+            LightVec = -1
+        }else{
+            LightVec = 1
+        }
+    };
 }
 
 function LightBack() {
@@ -89,41 +106,7 @@ function LightBack() {
     //lighthouse light end
 }
 
-function moveLight() {
-    var yelloT = vec4(1, 1, 0, 0.45);
-    renderCircle(3, 0.44, 0.48, yelloT, Math.PI * LAngle / 2 * LightVec, LSize)
-
-    console.log(LSizeF);
-
-    if (LSize == 10) {
-        LSizeF = false
-    }
-
-    if (LSize == 1 && !LSizeF) {
-        LSize = 2
-        LSizeF = true
-        LAngle = 1
-        LAngleF = true
-        console.log("tt");
-    }
-
-    if (LSizeF) {
-        LSize += 0.5
-    } else {
-        LSize -= 0.5
-    }
-
-    if (LAngleF) {
-        LAngle += 1 / 16
-    }
-
-    if (LAngle >= 3) {
-        LAngleF = false
-        LSize = 1
-    }
-
-}
-
+//구름
 function makeCloud(x, y, white) {
     renderCircle(0.1, 0.0 + x, 0.3 + y, white, Math.PI, 1)
     renderCircle(0.1, 0.15 + x, 0.33 + y, white, Math.PI, 1)
@@ -136,63 +119,7 @@ function makeCloud(x, y, white) {
     renderCircle(0.1, 0.3 + x, 0.2 + y, white, Math.PI, 1)
 }
 
-function makeColor(color, cnt) {
-    var tmp = [];
-    for (var c = 0; c < cnt; c++) {
-        tmp.push(color)
-    }
-    return tmp;
-}
-
-function drawFAN(a, b) {
-    gl.drawArrays(gl.TRIANGLE_FAN, a, b);
-}
-
-function drawTRI(a, b) {
-    gl.drawArrays(gl.TRIANGLES, a, b);
-}
-
-function makeBuffer(vertices, colors) {
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
-
-    var vertexPositionBufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
-
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    var vertexColorBufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
-}
-
-//draw circle
-function renderCircle(r, x, y, color, subAngle, size) {
-    var noOfFans = 200;
-    var centerOfCircle = vec2(x, y);
-    var anglePerFna = (2 * Math.PI) / noOfFans;
-    var mVirtices = [];
-    mVirtices.push(centerOfCircle);
-    for (var i = 0; i <= noOfFans / size; i++) {
-        var angle = anglePerFna * (i + 1);
-        mVirtices.push(
-            vec2(
-                x + Math.cos(angle + subAngle) * r,
-                y + Math.sin(angle + subAngle) * r
-            )
-        );
-    }
-    makeBuffer(mVirtices, makeColor(color, mVirtices.length))
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, mVirtices.length);
-}
-
+//마우스 리스너
 function onMouseUp(event) {
     var mx = event.clientX;
     var my = event.clientY;
---
+++ LightHouse/util.js
@@ -0,0 +1,92 @@
+function makeColor(color, cnt) {
+    var tmp = [];
+    for (var c = 0; c < cnt; c++) {
+        tmp.push(color)
+    }
+    return tmp;
+}
+
+function drawFAN(a, b) {
+    gl.drawArrays(gl.TRIANGLE_FAN, a, b);
+}
+
+function drawTRI(a, b) {
+    gl.drawArrays(gl.TRIANGLES, a, b);
+}
+
+//버퍼 만들기
+function makeBuffer(vertices, colors) {
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var vertexPositionBufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    var vertexColorBufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+}
+
+//draw circle
+function renderCircle(r, x, y, color, subAngle, size) {
+    var noOfFans = 200;
+    var centerOfCircle = vec2(x, y);
+    var anglePerFna = (2 * Math.PI) / noOfFans;
+    var mVirtices = [];
+    mVirtices.push(centerOfCircle);
+    for (var i = 0; i <= noOfFans / size; i++) {
+        var angle = anglePerFna * (i + 1);
+        mVirtices.push(
+            vec2(
+                x + Math.cos(angle + subAngle) * r,
+                y + Math.sin(angle + subAngle) * r
+            )
+        );
+    }
+    makeBuffer(mVirtices, makeColor(color, mVirtices.length))
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, mVirtices.length);
+}
+
+//빛 움직임
+function moveLight() {
+    var yelloT = vec4(1, 1, 0, 0.45);
+    renderCircle(3, 0.44, 0.48, yelloT, Math.PI * LAngle / 2 * LightVec, LSize)
+
+    console.log(LSizeF);
+
+    if (LSize == 10) {
+        LSizeF = false
+    }
+
+    if (LSize == 1 && !LSizeF) {
+        LSize = 2
+        LSizeF = true
+        LAngle = 1
+        LAngleF = true
+        console.log("tt");
+    }
+
+    if (LSizeF) {
+        LSize += 0.5
+    } else {
+        LSize -= 0.5
+    }
+
+    if (LAngleF) {
+        LAngle += 1 / 16
+    }
+
+    if (LAngle >= 3) {
+        LAngleF = false
+        LSize = 1
+    }
+}
